bbd285fdabaa17693b1c83c85596cbface2a488d
Atanu Karak <Atanu.Karak@vca.com>
2020-07-12 21:12:33 +0530

    Added test cases for Get promotional rules and refractored the code

--- PromotionalEngine.Tests/DataAccess/PromotionRuleDataAccessServiceTests.cs
+++ PromotionalEngine.Tests/DataAccess/PromotionRuleDataAccessServiceTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using PromotionalEngine.Common.DomainModels;
 using PromotionalEngine.DataAccess;
 using PromotionalEngine.DataAccess.DataStore;
+using System;
 using System.Collections.Generic;
 
 namespace PromotionalEngine.Tests.DataAccess
@@ -10,13 +11,18 @@ namespace PromotionalEngine.Tests.DataAccess
     [TestClass]
     public class PromotionRuleDataAccessServiceTests
     {
+        private readonly PromotionRuleDataAccessService _promoRuleDataAccessService;
+        private Mock<IStore> _mockDataStore;
+        public PromotionRuleDataAccessServiceTests()
+        {
+            _mockDataStore = new Mock<IStore>();
+            _promoRuleDataAccessService = new PromotionRuleDataAccessService(_mockDataStore.Object);
+        }
         [TestMethod]
         public void GetPromotionRules_NoRuleIsAddedInTheStore_ShouldReturnEmptyListForBothSingleAndComboRules()
         {
-            //Arrange
-            var mockDataStore = new Mock<IStore>();
-            var promoRuleDataAccessService = new PromotionRuleDataAccessService(mockDataStore.Object);
-            mockDataStore.SetupGet(x => x.PromotionRules).Returns(new PromotionRules
+            //Arrange            
+            _mockDataStore.SetupGet(x => x.PromotionRules).Returns(new PromotionRules
             {
                 SingleRules = new List<SinglePromoRule>()
              ,
@@ -24,12 +30,50 @@ namespace PromotionalEngine.Tests.DataAccess
             });
 
             //Act
-            PromotionRules result = promoRuleDataAccessService.GetPromotionRules();
+            PromotionRules result = _promoRuleDataAccessService.GetPromotionRules();
 
             //Assert
             Assert.IsNotNull(result);
             Assert.IsTrue(result.SingleRules.Count == 0);
             Assert.IsTrue(result.ComboRules.Count == 0);
+        }
+        [TestMethod]
+        public void GetPromotionRules_RulesAreAvailableInTheStore_ShouldNotReturnEmptyListForBothSingleAndComboRules()
+        {
+            //Arrange            
+            _mockDataStore.SetupGet(x => x.PromotionRules).Returns(new PromotionRules
+            {
+                SingleRules = new List<SinglePromoRule>()
+                {
+                    new SinglePromoRule{Item = new PromoRuleItem{}}
+                }
+             ,
+                ComboRules = new List<ComboPromoRule>()
+                {
+                   new ComboPromoRule
+                   {
+                       Items = new List<PromoRuleItem>()
+                   }
+                }
+            });
+
+            //Act
+            PromotionRules result = _promoRuleDataAccessService.GetPromotionRules();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.SingleRules.Count > 0);
+            Assert.IsTrue(result.ComboRules.Count > 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetPromotionRules_PromotionRuleStoreDoesNotExist_ShouldThrowNullReferenceException()
+        {
+            //Arrange            
+            //Act
+            _promoRuleDataAccessService.GetPromotionRules();
+            //Assert
+
         }
     }
 }
