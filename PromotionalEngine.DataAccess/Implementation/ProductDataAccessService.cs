94fba8174b3a3ac01a861743250d3dc1956dbd34
Atanu Karak <Atanu.Karak@vca.com>
2020-07-12 20:29:23 +0530

    Created UT to test the scenario when there is no product added into the product data store

---
+++ PromotionalEngine.DataAccess/Implementation/ProductDataAccessService.cs
@@ -0,0 +1,22 @@
+﻿using PromotionalEngine.Common.DomainModels;
+using PromotionalEngine.DataAccess.DataStore;
+using System;
+using System.Collections.Generic;
+
+namespace PromotionalEngine.DataAccess
+{
+    public class ProductDataAccessService
+    {
+        private IStore _dataStore;
+
+        public ProductDataAccessService(IStore dataStore)
+        {
+            _dataStore = dataStore;
+        }
+
+        public IList<ProductItem> GetProducts()
+        {
+            return _dataStore.Products;
+        }
+    }
+}
---
+++ PromotionalEngine.Tests/DataAccess/ProductDataAccessServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PromotionalEngine.Common.DomainModels;
+using PromotionalEngine.DataAccess;
+using PromotionalEngine.DataAccess.DataStore;
+using System.Collections.Generic;
+
+namespace PromotionalEngine.Tests.DataAccess
+{
+    [TestClass]
+    public class ProductDataAccessServiceTests
+    {
+        [TestMethod]
+        public void GetProducts_WhenNoProductInTheStore_ShouldReturnEmptyList()
+        {
+            //Arrange   
+            var mockDataStore = new Mock<IStore>();
+            var prodDataAccessService = new ProductDataAccessService(mockDataStore.Object);
+            mockDataStore.SetupGet(x => x.Products).Returns(new List<ProductItem>());
+            //Act
+            IList<ProductItem> result = prodDataAccessService.GetProducts();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count == 0);
+        }
+    }
+}
